1b0cb007baec0c0733ec7fa0e586570a893fd584
jairosl <jairo.lima@aluno.uepb.edu.br>
2021-06-29 20:27:46 -0300

    chore: add global reset css and create theme

---
+++ src/styles/global.ts
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from 'styled-components';
+
+const Styles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  body {
+    background: #fff;
+    color: ${props => props.theme.colors.primary};
+    font: 400 16px Roboto, sans-serif;
+  }
+`;
+
+export default Styles;
---
+++ src/styles/theme.ts
@@ -0,0 +1,12 @@
+const theme = {
+  colors: {
+    background: '#f8f8f8',
+    text: '#777777',
+    primary: '#228a95',
+    secondary: '#ef9c4b',
+    blue: '#187680',
+    orange: '#fd8030',
+  },
+};
+
+export default theme;
