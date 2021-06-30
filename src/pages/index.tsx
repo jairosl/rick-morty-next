a2a20c78584403dbd3f23170989b187277e3ea89
jairosl <jairo.lima@aluno.uepb.edu.br>
2021-06-30 16:18:44 -0300

    feat: add types for api response and use axios

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,40 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 import Header from '../components/Header';
 import Search from '../components/Search';
+import { api } from '../services/Api';
 
 import { Container } from '../styles/pages/home';
+
+interface Character {
+  id: number;
+  name: string;
+  species: string;
+  gender: string;
+  image: string;
+}
+
+interface ApiResponse {
+  info: {
+    count: number;
+    pages: number;
+    next: null | string;
+    prev: null | string;
+  };
+  results: Character[];
+}
 
 export default function Home() {
   const [searchValue, setSearchValue] = useState('');
 
   const handleChangeSearchValue = useCallback((data: string) => {
     setSearchValue(data);
+  }, []);
+
+  useEffect(() => {
+    api.get('/character').then(res => {
+      res.data as ApiResponse;
+    });
   }, []);
 
   return (
