b2971bae0ec8734036ae9e967f355fdb3a2cc8dc
jairosl <jairo.lima@aluno.uepb.edu.br>
2021-06-30 16:17:42 -0300

    refactor: move styles in the css of the Header Component

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -3,13 +3,6 @@ import styled from 'styled-components';
 export const Container = styled.div`
   // Small devices (landscape phones, 576px and up)
   @media (min-width: 275px) {
-    width: 100%;
-    height: 5em;
-    padding: 10px 20px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    background: ${props => props.theme.colors.background};
   }
 
   // Medium devices (tablets, 768px and up)
@@ -32,6 +25,14 @@ export const Container = styled.div`
       max-width: 1000px;
     }
   }
+
+  width: 100%;
+  height: 5em;
+  padding: 10px 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: ${props => props.theme.colors.background};
 
   header {
     display: flex;
