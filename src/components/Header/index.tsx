f926ed5c463e1740abfdc06cd5e5451fb93f8897
jairosl <jairo.lima@aluno.uepb.edu.br>
2021-07-03 11:57:18 -0300

    feat: use listFavorites in the header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,28 @@
+import { useState } from 'react';
 import { FiHeart } from 'react-icons/fi';
+
+import ListFavorites from '../ListFavorites';
+
 import { Container } from './styles';
 
 export default function Header() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleShowListFavorites = () => {
+    setIsVisible(true);
+  };
+
   return (
     <Container>
       <header>
         <h1>Rick &amp; Morty</h1>
-        <FiHeart size={30} />
+        <FiHeart size={30} onClick={handleShowListFavorites} />
+        {isVisible && (
+          <ListFavorites
+            isOpen={isVisible}
+            closeModal={() => setIsVisible(false)}
+          />
+        )}
       </header>
     </Container>
   );
