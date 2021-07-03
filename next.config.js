a4e3571c3887ab1b5ed42170285ac05bf366e97e
jairosl <jairo.lima@aluno.uepb.edu.br>
2021-07-03 14:46:27 -0300

    refactor: add ignore eslint in the deploy

--- next.config.js
+++ next.config.js
@@ -2,3 +2,11 @@ const withImages = require('next-images')
 module.exports = withImages({
   esModule: true,
 })
+
+module.exports = {
+  eslint: {
+    // Warning: Dangerously allow production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+}
